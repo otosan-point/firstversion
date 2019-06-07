6207aebbef196d03f3a567da085bbebc80c12f84
EC2 Default User <ec2-user@ip-172-31-1-159.ap-southeast-1.compute.internal>
2019-06-07 10:40:34 +0000

    beforeaction追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
-
+  before_action :authenticate_user! 
   def index
     @points = Action.where(approve: '2').count
     @promises = Promise.includes(:user).page(params[:page]).per(5).order("created_at DESC")
