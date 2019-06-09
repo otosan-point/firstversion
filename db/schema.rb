fd0dc34d59253312f3473fbdb087ee17eca544dc
EC2 Default User <ec2-user@ip-172-31-17-160.ap-southeast-1.compute.internal>
2019-06-09 05:58:57 +0000

    userテーブルにlast_access_kidカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_125726) do
+ActiveRecord::Schema.define(version: 2019_06_09_055329) do
 
   create_table "actions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "commemt"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_06_01_125726) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "last_access_kid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
