2a22fb9fe18bef9eda4f87cd4317ad20aa175477
EC2 Default User <ec2-user@ip-172-31-17-160.ap-southeast-1.compute.internal>
2019-06-11 01:59:51 +0000

    モデルkid,promise,actionにvalidationの設定を追加

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,3 +1,7 @@
 class Action < ApplicationRecord
   belongs_to :promise
+  
+#validation
+  validates_presence_of :promise_id, :kid_id
+
 end
--- app/models/promise.rb
+++ app/models/promise.rb
@@ -1,4 +1,8 @@
 class Promise < ApplicationRecord
   belongs_to :kid
   has_many :actions
+  
+  #validation
+  validates_presence_of :promise, :kid_id
+
 end
