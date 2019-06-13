cd9620bb7db39db21fa457c0626cc3f382ef867d
EC2 Default User <ec2-user@ip-172-31-17-160.ap-southeast-1.compute.internal>
2019-06-13 09:06:04 +0000

    合計ポイントの表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,7 +3,8 @@ class TopController < ApplicationController
   def index
 
     @kids = current_user.kids
-    @points = Action.where(approve: '2').count
+    @kid = Kid.find(current_user.last_access_kid)
+    @points = Action.where(kid_id: current_user.last_access_kid).count
     @promises = Promise.includes(:user).page(params[:page]).per(5).order("created_at DESC")
 
   end
