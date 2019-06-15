1bb7911ea41711257bf57f035d6d27ee8aff092d
EC2 Default User <ec2-user@ip-172-31-17-160.ap-southeast-1.compute.internal>
2019-06-15 09:21:59 +0000

    お父さんのページデザイン作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @kids = @user.kids
-
+        # @promises = Promise.where(user_id: @user.id)
+        # @actions = Action.includes(:promise)
     end
     
 end
