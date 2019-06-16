583276335bbb3ff69fcc0ffa4a4cd8faa1151a5e
EC2 Default User <ec2-user@ip-172-31-1-159.ap-southeast-1.compute.internal>
2019-06-16 06:38:46 +0000

    中間

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
         #@kids = @user.kids
         @kids = current_user.kids
         @comment_kid = Action.where(kid_id: current_user.last_access_kid)  #子供のコメント
-        
         # @promises = Promise.where(user_id: @user.id)
         # @actions = Action.includes(:promise)
     end
