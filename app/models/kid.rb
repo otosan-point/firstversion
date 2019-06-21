057029584ea20dc742e473550c866191415b72b3
EC2 Default User <ec2-user@ip-172-31-17-160.ap-southeast-1.compute.internal>
2019-06-21 03:34:04 +0000

    お父さんページのデザイン作成（未完成ですが）

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,7 +1,7 @@
 class Kid < ApplicationRecord
   belongs_to :user
   has_many :promises, ->{ order("created_at DESC") }
-  has_many :actions
+  has_many :actions, ->{ order("created_at DESC") }
   
   #validation
   validates_presence_of :user_id, :name
